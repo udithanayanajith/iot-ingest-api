3ebb71c0fc022708a4068e50e84e3fcb040bb3be
udithanayanajith <udithanayanajith97@gmail.com>
2025-09-10 12:52:56 +0530

    initial commit

---
+++ src/main/java/com/iot_ingest_api/uditha97/Uditha97Application.java
@@ -0,0 +1,13 @@
+package com.iot_ingest_api.uditha97;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uditha97Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uditha97Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iot_ingest_api/uditha97/Uditha97ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iot_ingest_api.uditha97;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uditha97ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
